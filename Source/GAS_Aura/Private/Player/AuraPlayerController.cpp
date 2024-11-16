546f86ea9f2df845912463f7d54b01d4dad5aba1
ltangdz <2890096794@qq.com>
2024-11-17 04:08:18 +0800

    1.8 Aura Player Controller

--- Source/GAS_Aura/Private/Player/AuraPlayerController.cpp
+++ Source/GAS_Aura/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,27 @@
 
 #include "Player/AuraPlayerController.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);	//断言 检查AuraContect 出错则崩溃
+
+	//增强输入子系统 在程序中只有一个 可以添加输入映射上下文
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);	//输入映射 优先级
+
+	//显示鼠标
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
--- Source/GAS_Aura/Public/Player/AuraPlayerController.h
+++ Source/GAS_Aura/Public/Player/AuraPlayerController.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "../../../../../../../../Engine/Epic Games/UE_5.3/Engine/Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputMappingContext;
 /**
  * 
  */
@@ -14,4 +16,13 @@ class GAS_AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
