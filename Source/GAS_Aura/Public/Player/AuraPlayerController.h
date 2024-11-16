4ee171422c234ee7eaaf65f59daf2cdcc8610588
ltangdz <2890096794@qq.com>
2024-11-17 02:47:47 +0800

    1.7 Enhanced Input

---
+++ Source/GAS_Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright by wanf2004
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
