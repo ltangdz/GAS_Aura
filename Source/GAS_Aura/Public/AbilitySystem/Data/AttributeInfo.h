e02096c58cb008a88509c28c1a84cc0a20aaf05f
ltangdz <2890096794@qq.com>
2025-01-17 00:45:08 +0800

    8.13 Attribute Info Data Asset

---
+++ Source/GAS_Aura/Public/AbilitySystem/Data/AttributeInfo.h
@@ -0,0 +1,40 @@
+// Copyright by wanf2004
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AttributeInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraAttributeInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTag AttributeTag = FGameplayTag();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText AttributeName = FText();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText AttributeDescription = FText();
+
+	UPROPERTY(BlueprintReadOnly)
+	float AttributeValue = 0.0f;
+	
+};
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API UAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	FAuraAttributeInfo FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound = false) const;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FAuraAttributeInfo> AttributeInformation;
+};
