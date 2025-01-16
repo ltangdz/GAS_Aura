83cabc645d9c4831dbbdafb2d0db60be5d5aa437
ltangdz <2890096794@qq.com>
2025-01-17 01:32:52 +0800

    8.15 Aura Ability System Blueprint Library

---
+++ Source/GAS_Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,20 @@
+// Copyright by wanf2004
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
